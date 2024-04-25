78858d5d05ad4b614b561a037ff2d202a131ba3e
Tanner Bindrup <tannerbindrup@mail.weber.edu>
2024-04-24 19:38:14 -0600

    final

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,5 +2,3 @@
 require.context('../fonts/', true, /\.(eot|ttf|woff|woff2)$/i)
 require.context('../images/', true, /\.(png|jpg|jpeg|gif|svg)$/i)
 require("../stylesheets/main.scss")
-
-//TODO - Your ES6 JavaScript code (if any) goes here
